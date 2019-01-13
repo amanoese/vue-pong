03b320d8a9f7cf135e59ab34ee3f4c05389df9e1
Seito Taka <amanoese@gmail.com>
2019-01-13 19:43:13 +0900

    implement example

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import Vuesax from 'vuesax'
 import App from './App.vue'
 
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+import 'material-icons/iconfont/material-icons.css';
+
 Vue.config.productionTip = false
+Vue.use(Vuesax)
 
 new Vue({
   render: h => h(App),
