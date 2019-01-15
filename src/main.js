d6c268271d0954afce60d16852bc1b31b2bbf223
amanoese <amanoese@gmail.com>
2019-01-16 00:55:50 +0900

    create login icon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import './plugins/fontawesome'
 import Vuesax from 'vuesax'
 import App from './App.vue'
 
-import 'vuesax/dist/vuesax.css' //Vuesax styles
-import 'material-icons/iconfont/material-icons.css';
+import 'bootstrap-social/bootstrap-social.css';
 import router from './router'
 
 Vue.config.productionTip = false
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    login!
+    <hr>
+    <b-col class="mt-2">
+      <b-row class="justify-content-center">
+        <b-button class="btn-social btn-github">
+          <font-awesome-icon :icon="['fab','github']" class="m-1"/>Sign in with Github
+        </b-button>
+      </b-row>
+    </b-col>
+    <b-col class="mt-2">
+      <b-row class="justify-content-center">
+        <b-button class="btn-social btn-twitter">
+          <font-awesome-icon :icon="['fab','twitter']" class="m-1"/>Sign in with Twitter
+        </b-button>
+      </b-row>
+    </b-col>
   </div>
 </template>
 
@@ -11,3 +25,8 @@ export default {
   name: 'home'
 }
 </script>
+<style>
+.m-4px {
+  margin-top: 4px;
+}
+</style>
