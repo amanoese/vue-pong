4a53fc56a0994867f4f2de5c17cab474e46f1d26
Seito Taka <amanoese@gmail.com>
2019-01-19 05:38:54 +0900

    Revert "add setting"
    This reverts commit b458d33b20047f4173fc7dd8c58bf6f28f45d872.

--- src/plugins/vuefire.js
+++ src/plugins/vuefire.js
@@ -2,15 +2,14 @@ import Vue from 'vue'
 
 import VueFire from 'vuefire'
 import firebase from 'firebase'
-
+import config from '@/../config/firebase.json'
 
 // explicit installation required in module environments
 Vue.use(VueFire)
-firebase.initializeApp(JSON.parse(process.env.FIRE_CONFIG||'false')||import('@/../config/firebase.json'));
+firebase.initializeApp(config);
 
 const db = firebase.firestore()
 const settings = { timestampsInSnapshots: true }
-
 db.settings(settings)
 
 export { db }
