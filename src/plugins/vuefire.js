f6a2c7e2e2811f8c5c1ac2d12aff3965981bb2a3
Seito Taka <amanoese@gmail.com>
2019-01-19 05:19:20 +0900

    add setting

--- src/plugins/vuefire.js
+++ src/plugins/vuefire.js
@@ -6,10 +6,11 @@ import config from '@/../config/firebase.json'
 
 // explicit installation required in module environments
 Vue.use(VueFire)
-firebase.initializeApp(config);
+firebase.initializeApp(JSON.parse(process.env.FIRE_CONFIG||'false')||config);
 
 const db = firebase.firestore()
 const settings = { timestampsInSnapshots: true }
+
 db.settings(settings)
 
 export { db }
